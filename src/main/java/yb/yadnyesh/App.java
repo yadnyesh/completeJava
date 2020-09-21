0ff326bcbd01ec43365e6140d56a94f6c51a8d85
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-09-21 09:56:23 +0530

    Base Java Project, this one is time bound

---
+++ src/main/java/yb/yadnyesh/App.java
@@ -0,0 +1,13 @@
+package yb.yadnyesh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
