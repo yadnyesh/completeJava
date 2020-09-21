cc89b7b60acfc68bf6549f5a6004a4edbb57c675
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-09-21 15:15:39 +0530

    Working code with > Java8 supported changes

---
+++ src/main/java/yb/yadnyesh/youtube/venkat/Sample.java
@@ -0,0 +1,21 @@
+package yb.yadnyesh.youtube.venkat;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Sample {
+    public static void printIt(int number) {
+        System.out.println("p: " + number +"--"+ Thread.currentThread());
+    }
+
+    public static Integer transform(Integer i) {
+        return i * 2;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        numbers.parallelStream()
+                .map(e -> transform(e))
+                .forEachOrdered(e -> printIt(e));
+    }
+}
