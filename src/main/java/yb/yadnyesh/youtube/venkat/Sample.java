6ce5d3955fc3949e18ed05a782a0e6d4b534e4e7
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-09-21 15:24:52 +0530

    Filter runs in parallel;

--- src/main/java/yb/yadnyesh/youtube/venkat/Sample.java
+++ src/main/java/yb/yadnyesh/youtube/venkat/Sample.java
@@ -8,16 +8,32 @@ public class Sample {
         System.out.println("p: " + number +"--"+ Thread.currentThread());
     }
 
-    public static Integer transform(Integer i) {
-        return i * 2;
+    public static Integer transform(Integer number) {
+        System.out.println("t: " + number + "--" + Thread.currentThread());
+        sleep(1000);
+        return number * 2;
+    }
+
+    public static Boolean check(Integer number) {
+        System.out.println("c: " + number + "--" + Thread.currentThread());
+        sleep(1000);
+        return true;
     }
 
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         numbers.parallelStream()
-                .map(e -> transform(e))
-                //.forEach(e -> printIt(e));
+                .filter(e -> check(e))
                 .forEachOrdered(e -> printIt(e));
         //For Each Ordered guarantee the source stream should be ordered
+    }
+
+    private static boolean sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 }
