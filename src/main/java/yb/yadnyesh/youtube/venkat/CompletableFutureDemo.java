fa95e9c0ed6235164faa9172f358b24d6161d5f3
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-09-26 14:14:27 +0530

    CompletableFuture - Thread of Execution

--- src/main/java/yb/yadnyesh/youtube/venkat/CompletableFutureDemo.java
+++ src/main/java/yb/yadnyesh/youtube/venkat/CompletableFutureDemo.java
@@ -14,9 +14,14 @@ public class CompletableFutureDemo {
         return CompletableFuture.supplyAsync(()->compute());
     }
 
+    public static void printIt(int i) {
+        log.info("Printed: " + i + Thread.currentThread());
+    }
+
     private static int compute() {
         try {
-            sleep(1);
+            log.info("compute: " + Thread.currentThread());
+            sleep(1000);
             return 2;
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -25,7 +30,11 @@ public class CompletableFutureDemo {
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+        log.info("Main Thread: " +  Thread.currentThread());
         CompletableFuture<Integer> future = create();
+        sleep(100);
+        future
+          .thenAccept(data -> printIt(data));
 //        future.thenAccept(data -> log.info(data.toString()))
 //        .thenRun(() -> log.info("This never dies"))
 //        .thenRun(() -> log.info("Really...This never dies"))
