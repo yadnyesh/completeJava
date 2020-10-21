c832613e56cb2520da174d72bf28fefc551cbc3d
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-10-21 09:18:33 +0530

    Existing changes

--- src/main/java/yb/yadnyesh/udemy/in28minutes/junittesting/Misc.java
+++ src/main/java/yb/yadnyesh/udemy/in28minutes/junittesting/Misc.java
@@ -1,4 +1,13 @@
 package yb.yadnyesh.udemy.in28minutes.junittesting;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ForkJoinPool;
+
+@Slf4j
 public class Misc {
+    public static void main(String[] args) {
+        log.info(String.valueOf(Runtime.getRuntime().availableProcessors()));
+        log.info(ForkJoinPool.commonPool().toString());
+    }
 }
