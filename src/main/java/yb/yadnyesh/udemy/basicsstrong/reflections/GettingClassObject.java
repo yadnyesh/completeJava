81eda413fd8ce690b6b80790bd55f5860e9c149b
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-10-27 19:03:44 +0530

    Getting the Class Name from Object

--- src/main/java/yb/yadnyesh/udemy/basicsstrong/reflections/GettingClassObject.java
+++ src/main/java/yb/yadnyesh/udemy/basicsstrong/reflections/GettingClassObject.java
@@ -2,6 +2,9 @@ package yb.yadnyesh.udemy.basicsstrong.reflections;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 @Slf4j
 public class GettingClassObject {
 
@@ -9,5 +12,17 @@ public class GettingClassObject {
         Class<?> stringClassA = Class.forName("java.lang.String");
         Class<?> stringClassB = Class.forName("java.lang.String");
         System.out.println(stringClassA == stringClassB);
+
+        PrivateClass privateClass = new PrivateClass();
+        Class<? extends PrivateClass> newPrivateClass = privateClass.getClass();
+
+        Class<?> superclass = newPrivateClass.getSuperclass();
+
+        Class<?>[] interfaces = newPrivateClass.getInterfaces();
+
+        Method[] declaredMethods = newPrivateClass.getDeclaredMethods();
+        Arrays.stream(declaredMethods).forEach(System.out::println);
+
+        System.out.println(newPrivateClass.getName());
     }
 }
