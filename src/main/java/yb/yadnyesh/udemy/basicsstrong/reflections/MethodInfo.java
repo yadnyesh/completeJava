24fc2f0ada044b71b6cfa3bee47584db101d7f35
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-11-02 14:19:33 +0530

    Method call

--- src/main/java/yb/yadnyesh/udemy/basicsstrong/reflections/MethodInfo.java
+++ src/main/java/yb/yadnyesh/udemy/basicsstrong/reflections/MethodInfo.java
@@ -1,14 +1,21 @@
 package yb.yadnyesh.udemy.basicsstrong.reflections;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class MethodInfo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Entity entity = new Entity(10, "id");
         Class<? extends Entity> entityClass = entity.getClass();
 
         Method[] declaredMethods = entityClass.getDeclaredMethods();
         Arrays.stream(declaredMethods).forEach(System.out::println);
+
+        System.out.println(entity.getVal());
+        Method setVal = entityClass.getMethod("setVal", int.class);
+        setVal.invoke(entity, 20);
+        System.out.println(entity.getVal());
+
     }
 }
