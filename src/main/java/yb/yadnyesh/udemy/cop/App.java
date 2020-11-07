18ccaa598440d5f55b6eb0352667c4c79a236f44
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-11-07 15:25:35 +0530

    Creating and Starting a Thread with Runnable

--- src/main/java/yb/yadnyesh/udemy/cop/App.java
+++ src/main/java/yb/yadnyesh/udemy/cop/App.java
@@ -3,14 +3,14 @@ package yb.yadnyesh.udemy.cop;
 public class App {
 
     public static void main(String[] args) {
-        Runner runner = new Runner();
-        runner.start();
-        Runner runner2 = new Runner();
-        runner2.start();
+        Thread t1 = new Thread(new Runner());
+        Thread t2 = new Thread(new Runner());
+        t1.start();
+        t2.start();
     }
 }
 
-class Runner extends Thread {
+class Runner implements Runnable {
 
     public void run() {
         for (int i = 0; i <= 10; i++) {
