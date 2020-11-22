5978920cff45f4afba0fed56fe683c88f80b3c61
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-11-22 12:09:05 +0530

    Create Observable from Various sources

--- src/main/java/yb/yadnyesh/udemy/basicsstrong/reactive/CreateObservable.java
+++ src/main/java/yb/yadnyesh/udemy/basicsstrong/reactive/CreateObservable.java
@@ -2,7 +2,9 @@ package yb.yadnyesh.udemy.basicsstrong.reactive;
 
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.internal.operators.observable.ObservableError;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CreateObservable {
@@ -22,12 +24,21 @@ public class CreateObservable {
         Observable<Integer> just = Observable.just(1,2,3);
         just.subscribe(System.out::println);
 
-        List<String> stringList = List.of("Ram","Shyam","RadheShyam","Mathura");
+        List<String> stringList = new ArrayList<>();//List.of("Ram","Shyam","RadheShyam","Mathura");
+        stringList.add("Ram");
+        stringList.add("Shyam");
+        stringList.add("RadheShyam");
+        stringList.add("Mathura");
 
         Observable<String> fromIterable = Observable.fromIterable(stringList);
         fromIterable.subscribe(System.out::println);
 
         Observable.range(3, 10).subscribe(System.out::println);
+
+        Observable<String> deferObservable = Observable.defer(() -> Observable.fromIterable(stringList));
+        deferObservable.subscribe(System.out::println);
+        stringList.add("Paul");
+        deferObservable.subscribe(System.out::println);
     }
 
 }
