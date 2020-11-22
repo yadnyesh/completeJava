01da1da8bcaaeef63006a55dddf70f3343e76941
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-11-22 12:44:21 +0530

    Creating Observer

---
+++ src/main/java/yb/yadnyesh/udemy/basicsstrong/reactive/CreatingObserver.java
@@ -0,0 +1,10 @@
+package yb.yadnyesh.udemy.basicsstrong.reactive;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class CreatingObserver {
+    public static void main(String[] args) {
+        Observable<String> source = Observable.just("Orange","Black","Red");
+        source.subscribe(e -> System.out.println(e), Throwable::printStackTrace, () -> System.out.println("Completed"));
+    }
+}
